6986878cb93fb0715f2dd45ada35622d1dc0b963
Allan Espinosa <aespinosa@engineyard.com>
2016-05-25 07:42:35 +0900

    Use the tarball installation method
    Needed to get 1.11.0 onwards

--- test/cookbooks/main/recipes/default.rb
+++ test/cookbooks/main/recipes/default.rb
@@ -1,5 +1,18 @@
-docker_installation_binary 'default' do
+# FIXME: propose as a new Custom resource upstream
+tarball = remote_file "#{Chef::Config[:file_cache_path]}/docker-1.11.1.tgz" do
+  source 'https://get.docker.com/builds/Linux/x86_64/docker-1.11.1.tgz'
+  checksum '893e3c6e89c0cd2c5f1e51ea41bc2dd97f5e791fcfa3cee28445df277836339d'
+  notifies :run, 'execute[extract tarball]', :immediately
   only_if { node['instance_role'] == 'util' && node['name'] == 'docker' }
+end
+
+execute 'extract tarball' do
+  command "tar xzf #{tarball.path} --strip-components=1 -C /usr/bin"
+  action :nothing
+  %w(/usr/bin/docker /usr/bin/docker-containerd /usr/bin/docker-containerd-ctr
+     /usr/bin/docker-runc /usr/bin/docker-contianerd-shim).each do |file|
+    creates file
+  end
 end
 
 docker_service_manager 'default' do
