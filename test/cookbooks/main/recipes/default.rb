e1cb39b692798cb0dbaf5ed3010e4d89f5ab755b
Allan Espinosa <aespinosa@engineyard.com>
2016-05-26 08:42:37 +0900

    Write update about expected changes

--- test/cookbooks/main/recipes/default.rb
+++ test/cookbooks/main/recipes/default.rb
@@ -1,4 +1,10 @@
-# FIXME: propose as a new Custom resource upstream
+# FIXME: Submitted as a pull request in [1]. Wait for it to be merged and
+# released then update as follows:
+# docker_installation_tarball 'default' do
+#   only_if { node['instance_role'] == 'util' && node['name'] == 'docker' }
+# end
+#
+# [1] https://github.com/chef-cookbooks/docker/pull/712
 tarball = remote_file "#{Chef::Config[:file_cache_path]}/docker-1.11.1.tgz" do
   source 'https://get.docker.com/builds/Linux/x86_64/docker-1.11.1.tgz'
   checksum '893e3c6e89c0cd2c5f1e51ea41bc2dd97f5e791fcfa3cee28445df277836339d'
